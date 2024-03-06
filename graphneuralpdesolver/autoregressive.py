80ccfbda0f319c07e62b624ef9f799abc917cc16
Sepehr Mousavi <mousavi.sep@gmail.com>
2024-03-06 18:43:30 +0100

    improve the combined autoregressive prediction by using jax.lax.scan twice

--- graphneuralpdesolver/autoregressive.py
+++ graphneuralpdesolver/autoregressive.py
@@ -22,19 +22,19 @@ class AutoregressivePredictor:
 
     time_deltas = (1. + jnp.arange(self.num_steps_direct)).reshape(-1, 1)
 
-    def scan_fn(u_inp, forcing):
-      u_out = jax.vmap(
-        lambda dt: self._apply_operator(variables, specs=specs, u_inp=u_inp, dt=dt)
-      )(time_deltas)
-      u_out = jnp.squeeze(u_out, axis=2).swapaxes(0, 1)
-      u_inp_next = u_out[:, -1:]  # Take the last time step as the next input
-      rollout = jnp.concatenate([u_inp, u_out[:, :-1]], axis=1)
-      return u_inp_next, rollout
+    def scan_fn_direct(u_inp, dt):
+      u_out = self._apply_operator(variables, specs=specs, u_inp=u_inp, dt=dt)
+      return u_out, u_inp
 
-    # CHECK: Use flax.linen.scan for the for loops?
+    def scan_fn_autoregressive(u_inp, forcing):
+      u_next, u_out = jax.lax.scan(f=scan_fn_direct,
+        init=u_inp, xs=time_deltas, length=self.num_steps_direct)
+      u_out = jnp.squeeze(u_out, axis=2).swapaxes(0, 1)
+      return u_next, u_out
+
     forcings = None
-    u_next, rollout = jax.lax.scan(
-      f=scan_fn, init=u_inp, xs=forcings, length=(num_steps // self.num_steps_direct))
+    u_next, rollout = jax.lax.scan(f=scan_fn_autoregressive,
+      init=u_inp, xs=forcings, length=(num_steps // self.num_steps_direct))
     rollout = rollout.swapaxes(0, 1)
     rollout = rollout.reshape(batch_size, num_steps, num_grid_nodes, num_outputs)
 
@@ -42,10 +42,6 @@ class AutoregressivePredictor:
 
   def jump(self, variables: flax.typing.VariableDict,
     specs: Array, u_inp: Array, num_steps: int) -> Array:
-
-    batch_size = u_inp.shape[0]
-    num_grid_nodes = u_inp.shape[2]
-    num_outputs = u_inp.shape[3]
 
     time_deltas = jnp.array(self.num_steps_direct).reshape(-1, 1)
 
